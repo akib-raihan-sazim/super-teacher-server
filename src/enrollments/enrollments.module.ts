d944cced1f801f56fdd361cd3179ade3d6367377
Akib Raihan <akib.raihan@sazim.io>
2024-09-05 21:17:09 +0600

    feat(ST-105): sent mail when student enroll in a class

--- src/enrollments/enrollments.module.ts
+++ src/enrollments/enrollments.module.ts
@@ -5,12 +5,13 @@ import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Classroom } from "@/common/entities/classrooms.entity";
 import { Enrollment } from "@/common/entities/enrollments.entity";
 import { Student } from "@/common/entities/students.entity";
+import { MailModule } from "@/mail/mail.module";
 
 import { EnrollmentsController } from "./enrollments.controller";
 import { EnrollmentsService } from "./enrollments.service";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Classroom, Student, Enrollment])],
+  imports: [MikroOrmModule.forFeature([Classroom, Student, Enrollment]), MailModule],
   providers: [EnrollmentsService],
   controllers: [EnrollmentsController],
 })
