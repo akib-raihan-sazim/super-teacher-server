ffdb3065128a2c52aed2f24c71b242b6877f1452
Akib Raihan <akib.raihan@sazim.io>
2024-09-06 12:46:35 +0600

    feat(ST-108): fetch classroom for specific student

--- src/enrollments/enrollments.repository.ts
+++ src/enrollments/enrollments.repository.ts
@@ -5,4 +5,9 @@ import { EntityRepository } from "@mikro-orm/postgresql";
 import { Enrollment } from "@/common/entities/enrollments.entity";
 
 @Injectable()
-export class EnrollmentsRepository extends EntityRepository<Enrollment> {}
+export class EnrollmentsRepository extends EntityRepository<Enrollment> {
+  async getClassroomIdsForStudent(studentId: number): Promise<number[]> {
+    const enrollments = await this.find({ student: studentId });
+    return enrollments.map((enrollment) => enrollment.classroom.id);
+  }
+}
