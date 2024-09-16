e03a3bbbef058907123ea562d37e03f09446d0b8
Akib Raihan <akib.raihan@sazim.io>
2024-09-16 20:35:02 +0600

    feat(ST-122): create and migrate resource table

---
+++ src/common/entities/resources.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryKey,
+  Property,
+  ManyToOne,
+  EntityRepositoryType,
+  Rel,
+} from "@mikro-orm/core";
+
+import { ResourcesRepository } from "@/resources/resources.repository";
+
+import { Classroom } from "./classrooms.entity";
+import { CustomBaseEntity } from "./custom-base.entity";
+
+@Entity({ tableName: "resources", repository: () => ResourcesRepository })
+export class Resources extends CustomBaseEntity {
+  [EntityRepositoryType]?: ResourcesRepository;
+
+  @PrimaryKey()
+  id!: number;
+
+  @Property({ fieldName: "title" })
+  title!: string;
+
+  @Property({ fieldName: "file_url" })
+  fileUrl!: string;
+
+  @ManyToOne(() => Classroom)
+  classroom!: Rel<Classroom>;
+}
