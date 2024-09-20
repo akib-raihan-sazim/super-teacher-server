12375ea9ec81da387379b3a08f730749ac56394e
Akib Raihan <akib.raihan@sazim.io>
2024-09-20 18:07:06 +0600

    feat(ST-141): edit assignment endpoint

--- src/assignments/assignments.repository.ts
+++ src/assignments/assignments.repository.ts
@@ -4,12 +4,21 @@ import { EntityRepository } from "@mikro-orm/postgresql";
 
 import { Assignment } from "@/common/entities/assignments.entity";
 
-import { CreateAssignmentDto } from "./assignments.dtos";
+import { CreateAssignmentDto, UpdateAssignmentDto } from "./assignments.dtos";
 
 @Injectable()
 export class AssignmentsRepository extends EntityRepository<Assignment> {
   async createOne(createAssignmentDto: CreateAssignmentDto): Promise<Assignment> {
     const assignment = this.create(createAssignmentDto);
+    await this.em.persistAndFlush(assignment);
+    return assignment;
+  }
+
+  async updateOne(
+    assignment: Assignment,
+    updateAssignmentDto: UpdateAssignmentDto,
+  ): Promise<Assignment> {
+    this.assign(assignment, updateAssignmentDto);
     await this.em.persistAndFlush(assignment);
     return assignment;
   }
--- src/assignments/assignments.service.ts
+++ src/assignments/assignments.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@nestjs/common";
 import { ClassroomsRepository } from "@/classrooms/classrooms.repository";
 import { FileUploadsService } from "@/file-uploads/file-uploads.service";
 
-import { CreateAssignmentDto, UploadAssignmentDto } from "./assignments.dtos";
+import { CreateAssignmentDto, UpdateAssignmentDto, UploadAssignmentDto } from "./assignments.dtos";
 import { AssignmentsRepository } from "./assignments.repository";
 
 @Injectable()
@@ -43,5 +43,25 @@ export class AssignmentsService {
   async getAssignmentsByClassroomId(classroomId: number) {
     const classroom = await this.classroomsRepository.findOneOrFail({ id: classroomId });
     return this.assignmentsRepository.find({ classroom });
+  }
+
+  async updateAssignment(
+    assignmentId: number,
+    updateAssignmentDto: UpdateAssignmentDto,
+    file?: Express.Multer.File,
+  ) {
+    const assignment = await this.assignmentsRepository.findOneOrFail(assignmentId);
+    if (file) {
+      const oldFileKey = assignment.fileUrl.split("project-dev-bucket/")[1];
+      await this.fileUploadsService.deleteFromS3(oldFileKey);
+      const presignedUrl = await this.fileUploadsService.getPresignedUrl(file);
+      const uploadResponse = await this.fileUploadsService.uploadToS3(presignedUrl, file);
+      if (!uploadResponse.ok) {
+        throw new Error("Failed to upload file to S3");
+      }
+      updateAssignmentDto.fileUrl = presignedUrl.split("?")[0];
+    }
+
+    return this.assignmentsRepository.updateOne(assignment, updateAssignmentDto);
   }
 }
