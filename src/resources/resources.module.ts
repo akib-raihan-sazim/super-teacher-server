2fe2dc2e18ee96cc5a037e643bca846f6c2ecd35
Akib Raihan <akib.raihan@sazim.io>
2024-09-25 18:08:02 +0600

    feat(ST-165): add notify students feature to exams and resources

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -3,14 +3,20 @@ import { Module } from "@nestjs/common";
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 
 import { Classroom } from "@/common/entities/classrooms.entity";
+import { Enrollment } from "@/common/entities/enrollments.entity";
 import { Resources } from "@/common/entities/resources.entity";
 import { FileUploadsModule } from "@/file-uploads/file-uploads.module";
+import { MailModule } from "@/mail/mail.module";
 
 import { ResourcesController } from "./resources.controller";
 import { ResourcesService } from "./resources.service";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Resources, Classroom]), FileUploadsModule],
+  imports: [
+    MikroOrmModule.forFeature([Resources, Classroom, Enrollment]),
+    FileUploadsModule,
+    MailModule,
+  ],
   controllers: [ResourcesController],
   providers: [ResourcesService],
 })
