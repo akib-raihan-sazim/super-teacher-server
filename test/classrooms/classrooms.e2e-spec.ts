1e0b12b6c0bad2683981b5733045d375325a36fc
Akib Raihan <akib.raihan@sazim.io>
2024-10-16 17:03:12 +0600

    feat(ST-183): add test for get classroom by id

--- test/classrooms/classrooms.e2e-spec.ts
+++ test/classrooms/classrooms.e2e-spec.ts
@@ -4,6 +4,7 @@ import { EntityManager, IDatabaseDriver, Connection, MikroORM } from "@mikro-orm
 
 import request from "supertest";
 
+import { Classroom } from "@/common/entities/classrooms.entity";
 import { User } from "@/common/entities/users.entity";
 
 import { bootstrapTestServer } from "../utils/bootstrap";
@@ -11,6 +12,7 @@ import { truncateTables } from "../utils/db";
 import { ClassroomFactory } from "../utils/factories/classrooms.factory";
 import { UserFactory } from "../utils/factories/users.factory";
 import { getAccessToken } from "../utils/helpers/access-token.helpers";
+import { createClassroomInDb } from "../utils/helpers/create-classroom-in-db.helpers";
 import { createTeacherInDb, createStudentInDb } from "../utils/helpers/create-user-in-db.helpers";
 import { THttpServer } from "../utils/types";
 
@@ -109,6 +111,73 @@ describe("ClassroomsController (e2e)", () => {
         .set("Authorization", `Bearer ${teacherToken}`)
         .send(invalidClassroomDto)
         .expect(HttpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe("GET /classrooms/:id", () => {
+    let teacherUser: User;
+    let teacherToken: string;
+    let classroom: Classroom;
+
+    beforeEach(async () => {
+      const {
+        user: teacherUserData,
+        teacher,
+        plainTextPassword: teacherPassword,
+      } = await UserFactory.createTeacher();
+      teacherUser = await createTeacherInDb(dbService, teacherUserData, teacher);
+      teacherToken = await getAccessToken(httpServer, teacherUser.email, teacherPassword);
+
+      const classroomDto = ClassroomFactory.createClassroomDto();
+
+      classroom = await createClassroomInDb(dbService, teacherUser.teacher!, classroomDto);
+    });
+
+    it("should get classroom by ID successfully when teacher is authenticated", async () => {
+      const response = await request(httpServer)
+        .get(`/classrooms/${classroom.id}`)
+        .set("Authorization", `Bearer ${teacherToken}`)
+        .expect(HttpStatus.OK);
+
+      expect(response.body).toMatchObject({
+        id: classroom.id,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        title: classroom.title,
+        subject: classroom.subject,
+        classTime: classroom.classTime.toISOString(),
+        days: classroom.days,
+        teacher: {
+          id: expect.any(Number),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          highestEducationLevel: teacherUser.teacher?.highestEducationLevel,
+          majorSubject: teacherUser.teacher?.majorSubject,
+          subjectsToTeach: teacherUser.teacher?.subjectsToTeach,
+          user: {
+            id: teacherUser.id,
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            firstName: teacherUser.firstName,
+            lastName: teacherUser.lastName,
+            email: teacherUser.email,
+            gender: teacherUser.gender,
+            userType: teacherUser.userType,
+          },
+        },
+      });
+    });
+
+    it("should return 401 Unauthorized when no token is provided", async () => {
+      await request(httpServer).get(`/classrooms/${classroom.id}`).expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it("should return 404 Not Found when classroom doesn't exist", async () => {
+      const nonExistentId = 9999;
+      await request(httpServer)
+        .get(`/classrooms/${nonExistentId}`)
+        .set("Authorization", `Bearer ${teacherToken}`)
+        .expect(HttpStatus.NOT_FOUND);
     });
   });
 });
